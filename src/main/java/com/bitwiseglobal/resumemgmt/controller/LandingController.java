0dbbbbf460c53b53ba3b8bb164b5229691d97cd2
Mangesh Jogade <mangesh.mpj@gmail.com>
2017-09-20 20:56:01 -0500

    Temp changes to test table insertion and retrieval

--- src/main/java/com/bitwiseglobal/resumemgmt/controller/LandingController.java
+++ src/main/java/com/bitwiseglobal/resumemgmt/controller/LandingController.java
@@ -5,10 +5,13 @@ package com.bitwiseglobal.resumemgmt.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitwiseglobal.resumemgmt.bd.ResumeMgmtBD;
 
 /**
  * @author Chetan Menge
@@ -18,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LandingController {
 	
 	private static final Logger logger=LoggerFactory.getLogger(LandingController.class);
+	
+	@Autowired
+	ResumeMgmtBD resumeMgmtBD;
 	
 	@RequestMapping(value="/rmLanding",method=RequestMethod.GET)
 	public String landing(Model model) {
@@ -29,4 +35,22 @@ public class LandingController {
 		return "index";	
 	}
 
+	@RequestMapping(value="/addSkill",method=RequestMethod.GET)
+	public String addSkill() {
+		resumeMgmtBD.addSkill();
+		return "temp";	
+	}
+	
+	@RequestMapping(value="/addResume",method=RequestMethod.GET)
+	public String addResume() {
+		resumeMgmtBD.addResume();
+		return "temp";	
+	}
+	
+	@RequestMapping(value="/addUser",method=RequestMethod.GET)
+	public String addUser() {
+		resumeMgmtBD.addUser();
+		return "temp";	
+	}
+	
 }
--- src/main/java/com/bitwiseglobal/resumemgmt/controller/LoginController.java
+++ src/main/java/com/bitwiseglobal/resumemgmt/controller/LoginController.java
@@ -4,9 +4,12 @@
 package com.bitwiseglobal.resumemgmt.controller;
 
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bitwiseglobal.resumemgmt.bd.ResumeMgmtBD;
 
 /**
  * @author Chetan Menge
@@ -16,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginController {
 
 	private static final Logger logger=Logger.getLogger(LoginController.class);
+	
+	@Autowired
+	ResumeMgmtBD resumeMgmtBD;
 	
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String login() {
